5a202464b9d2ad7dfe052bde0db1fae86c1d2d80
amymc <amy.mccarthy2@mail.dcu.ie>
2016-10-19 14:10:00 +0200

    change root url for hosting

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <ImageViewer/>
+        <Match exactly pattern="/build/" component={ImageViewer} />
       </div>
     </BrowserRouter>
   )
